de2506c2b8662c40275cf1ecf66f90a5f2cb2eca
poarfrippe <philipp.gruber02@gmail.com>
2021-03-12 17:03:08 +0100

    Base Structure

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,21 @@
-console.log("hallo sepp")
+const express = require('express');
+const app = express();
+
+const ip = "localhost"
+const port = 12345
+
+app.get('/', (req, res) =>{
+    res.sendFile("Client/index.html", {root: "./"})
+})
+
+app.get("/script.js", (req, res) => {
+    res.sendFile("Client/script.js", {root: "./"})
+})
+
+app.get("/style.css", (req, res) => {
+    res.sendFile("Client/style.css", {root: "./"})
+})
+
+app.listen(port, () => {
+    console.log("Server running on: http://" + ip + ":" + port)
+})
