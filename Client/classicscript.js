54d03efb16d360eb22a813d851e786bced017a6f
poarfrippe <philipp.gruber02@gmail.com>
2021-03-19 19:40:08 +0100

    schneidiges Aimen

--- Client/classicscript.js
+++ Client/classicscript.js
@@ -11,11 +11,16 @@ const raycaster = new THREE.Raycaster();
 
 let camx = 0
 let camy = 0
+let camz = 0
+let anglex = 0
+let angley = 0
 
 const roomwidth = 50
 const roomheight = 40
 const roomdepth = 40
+const roomhintercam = 20
 
+const wholeround = 2*Math.PI
 
 window.onload = function() {
 
@@ -49,23 +54,42 @@ function onbodyclick(event) {
 function handleMouseMove(event) {
 
     if(document.pointerLockElement == document.body) {
-        if ((event.movementX < 0 && (camx - event.movementX / 1000) < 1.3) || (event.movementX > 0 && (camx - event.movementX / 1000) > -1.3)) {
-            camx -= (event.movementX / 1000)
-        }
-        if ((event.movementY < 0 && (camy - event.movementY / 1000) < 1.3) || (event.movementY > 0 && (camy - event.movementY / 1000) > -1.3)) {
-            camy -= (event.movementY / 1000)
-        }
         
-        //console.log("x= "+camx)
-        //console.log("y= "+camy)
+        if(anglex > wholeround){
+            anglex = 0
+        } else if(anglex < 0) {
+            anglex = wholeround
+        }
+
+        if(angley > wholeround){
+            angley = 0
+        } else if(angley < 0) {
+            angley = wholeround
+        }
+
+        anglex += event.movementX / 1000
+        angley += event.movementY / 1000
+
+        //soll Eine Sphere um (0,0,0) darstellen wo je nach x winkel und y winkel der x, y, und z punkt ausgerechnt wird wo die kamera hinsehen soll!!!!
+        camx = getgegenkathete(anglex)
+        camy = getgegenkathete(angley)
+        let sepp = getankathete(angley)                 //ist dazu da dass z bei bewegungen entlang der y achse nicht gleich bleibt
+        camz = getgegenkathete(wholeround/4-anglex, sepp)
+        
     } else {
         //console.log('pointer net gelockt!');
     }
 
-    camera.rotation.y = camx
-    camera.rotation.x = camy
+    camera.lookAt(camx, -camy, -camz)
 
+}
 
+function getgegenkathete(angle, hyplenght = 1) {
+    return Math.sin(angle)*hyplenght
+}
+
+function getankathete(angle, hyplenght = 1) {
+    return Math.cos(angle)*hyplenght
 }
 
 function drawroom(width, height, depth) {
@@ -73,7 +97,7 @@ function drawroom(width, height, depth) {
     //rechts
     for (let i = -5; i < 6; ++i) {
         let points = [];
-        points.push( new THREE.Vector3( width/2, i*height/10, 20 ) );
+        points.push( new THREE.Vector3( width/2, i*height/10, roomhintercam ) );
         points.push( new THREE.Vector3( width/2, i*height/10, -depth ) );
         drawline(points)
     }
@@ -82,7 +106,7 @@ function drawroom(width, height, depth) {
     //links
     for (let i = -5; i < 5; ++i) {
         let points = [];
-        points.push( new THREE.Vector3( -width/2, i*height/10, 20 ) );
+        points.push( new THREE.Vector3( -width/2, i*height/10, roomhintercam ) );
         points.push( new THREE.Vector3( -width/2, i*height/10, -depth ) );
         drawline(points)
     }
@@ -98,15 +122,15 @@ function drawroom(width, height, depth) {
     //hinte
     for (let i = -5; i < 5; ++i) {
         let points = [];
-        points.push( new THREE.Vector3( width/2, i*height/10, 20 ) );
-        points.push( new THREE.Vector3( -width/2, i*height/10, 20 ) );
+        points.push( new THREE.Vector3( width/2, i*height/10, roomhintercam ) );
+        points.push( new THREE.Vector3( -width/2, i*height/10, roomhintercam ) );
         drawline(points)
     }
 
     //unten
     for (let i = -5; i < 5; ++i) {
         let points = [];
-        points.push( new THREE.Vector3( i*width/10, -height/2, 20 ) );
+        points.push( new THREE.Vector3( i*width/10, -height/2, roomhintercam ) );
         points.push( new THREE.Vector3( i*width/10, -height/2, -depth ) );
         drawline(points)
     }
@@ -114,7 +138,7 @@ function drawroom(width, height, depth) {
     //oben
     for (let i = -5; i < 5; ++i) {
         let points = [];
-        points.push( new THREE.Vector3( i*width/10, height/2, 20 ) );
+        points.push( new THREE.Vector3( i*width/10, height/2, roomhintercam ) );
         points.push( new THREE.Vector3( i*width/10, height/2, -depth ) );
         drawline(points)
     }
@@ -128,20 +152,65 @@ function drawroom(width, height, depth) {
     }
 
 
+    let material = new THREE.MeshToonMaterial({color: "#4B6E6B"})
+    let geometry
+    let mesh
+
+    //wand rechts
+    geometry = new THREE.BoxGeometry(1, roomheight+1, roomdepth+roomhintercam+2)
+    mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = roomwidth/2+1
+    mesh.position.y = 0
+    mesh.position.z = -roomdepth+((roomdepth+roomhintercam)/2)
+    scene.add(mesh)
+
+    //wand links
+    geometry = new THREE.BoxGeometry(1, roomheight+1, roomdepth+roomhintercam+2)
+    mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = -roomwidth/2-1
+    mesh.position.y = 0
+    mesh.position.z = -roomdepth+((roomdepth+roomhintercam)/2)
+    scene.add(mesh)
+
+    //wand forne
+    geometry = new THREE.BoxGeometry(roomwidth+1, roomheight+1, 1)  //jo isch schun gessepplt also sepp... i muas fortniten...
+    mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = 0
+    mesh.position.y = 0
+    mesh.position.z = -roomdepth-1
+    scene.add(mesh)
+
+    //wand hinten
+    geometry = new THREE.BoxGeometry(roomwidth+1, roomheight+1, 1)  //jo isch schun gessepplt also sepp... i muas fortniten...
+    mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = 0
+    mesh.position.y = 0
+    mesh.position.z = roomhintercam+1
+    scene.add(mesh)
+
+    //boden
+    geometry = new THREE.BoxGeometry(roomwidth+1, 1, roomdepth+roomhintercam+2)  //jo isch schun gessepplt also sepp... i muas fortniten...
+    mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = 0
+    mesh.position.y = -(roomheight/2)-1
+    mesh.position.z = -roomdepth+((roomdepth+roomhintercam)/2)
+    scene.add(mesh)
+
+    //decke
+    geometry = new THREE.BoxGeometry(roomwidth+1, 1, roomdepth+roomhintercam+2)  //jo isch schun gessepplt also sepp... i muas fortniten...
+    mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = 0
+    mesh.position.y = +(roomheight/2)+1
+    mesh.position.z = -roomdepth+((roomdepth+roomhintercam)/2)
+    scene.add(mesh)
 
 }
 
 function drawline(points) {
-
     const material = new THREE.LineBasicMaterial( { color: 0x7D00FF } );
-
-
     const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
     const line = new THREE.Line( geometry, material );
-
     scene.add( line );
-
 }
 
 function inittargets(howmanny) {
@@ -181,14 +250,13 @@ function onMouseClick(event) {
 
     event.preventDefault();
 
-    raycaster.setFromCamera(crosshair, camera);
+    console.log("x= " + camx)
+    console.log("y= " + camy)
 
-    let intersects = raycaster.intersectObjects(scene.children, true)
-
-    console.log(intersects[0].object)
-
+    //raycaster.setFromCamera(crosshair, camera);
+    //let intersects = raycaster.intersectObjects(scene.children, true)
+    //console.log(intersects[0].object)
     //console.log(intersects)
-
     /*
     for (var i = 0; i < intersects.length; i++) {
         intersects[i].object.material.color = {r: 0, g: 255, b: 0}
