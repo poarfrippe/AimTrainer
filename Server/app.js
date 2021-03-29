e3dc636ed403716f7b25e71c0d5deda19d6ed1c8
SparberFabian <sparberfabian2002@gmail.com>
2021-03-29 17:12:13 +0200

    Datenbankverbindung register

--- Server/app.js
+++ Server/app.js
@@ -8,6 +8,20 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 const ip = "localhost"
 const port = 36187
 
+//databse connection
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "aimtrainer"
+})
+if(con){
+    console.log('Connection Success du fock');
+} else {
+    console.log('Cant connect to db, Check ur db connection du fock');
+}
+
 //home
 app.get('/', (req, res) =>{
     res.sendFile("Client/home/index.html", {root: "./"})
@@ -35,9 +49,12 @@ app.get("/login/loginregister.css", (req, res) => {
 })
 app.post("/login", (req, res) => {
     //sparber schau, des geat oanfoch so und des iatz oanfoch of die datenbank... Blessed!!
+    var username = req.body.username
+    var password = req.body.pswdli
+
     console.log(req.body.username)
     console.log(req.body.pswdli)
-    res.redirect("http://localhost:36187/", 302)
+    res.redirect(302, "http://localhost:36187/")
 })
 
 //registere
@@ -46,6 +63,22 @@ app.get("/register", (req, res) => {
 })
 app.get("/register/loginregister.css", (req, res) => {
     res.sendFile("Client/loginRegister/loginregister.css", {root: "./"})
+})
+app.post("/register", (req, res) =>{
+    let username = req.body.username
+    let password = req.body.pswdli
+    let email = req.body.email
+
+    let sql = "INSERT INTO benutzer (`email`, `username`, `passwort`) VALUES ('"+email+"', '"+username+"', '"+password+"')";
+    con.query(sql, function (err, result){
+        if (err) throw err;
+        console.log("1 record inserted");
+    })
+
+    console.log(req.body.username)
+    console.log(req.body.pswdli)
+    console.log(req.body.email)
+    res.redirect(302, "http://localhost:36187/")
 })
 
 
@@ -78,6 +111,7 @@ app.post("/classic", (req, res) => {
     //des no of DB
     console.log("hallo, bin im post iatz")
     console.log("score wos in die datenbank zu seppln muas: " + req.body.score)
+    var score = req.body.score
     res.send("hoi, hon gekreag, jo!!!")
 })
 
