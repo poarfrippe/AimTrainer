b8f017d97578034c40ae989bb6e3a82d82c180b4
SparberFabian <sparberfabian2002@gmail.com>
2021-04-05 18:29:02 +0200

    register funktioniert

--- Server/app.js
+++ Server/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-let bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const ip = "localhost"
 const port = 36187
+i = 0;
 
 //databse connection
 var mysql = require('mysql');
@@ -17,10 +17,15 @@ var con = mysql.createConnection({
     database: "aimtrainer"
 })
 if(con){
-    console.log('Connection Success du fock');
+    console.log('Connection Success');
 } else {
-    console.log('Cant connect to db, Check ur db connection du fock');
+    console.log('Cant connect to db, Check ur db connection');
 }
+
+con.query("SELECT COUNT(*) AS usernameCount FROM benutzer", function (err, rows, fields){
+    if(err) throw err;
+    i = rows[0].usernameCount        
+});
 
 //home
 app.get('/', (req, res) =>{
@@ -52,6 +57,21 @@ app.post("/login", (req, res) => {
     var username = req.body.username
     var password = req.body.pswdli
 
+    con.query("SELECT `username`, `passwort` FROM benutzer", function (err, result, fields) {
+        if(err) throw err;
+        while(i<10){
+            if(typeof(result[i]) == 'undefined'){
+                break;
+            }
+            if(result[i].username !== username && result[i].password !== password){
+                console.log("hot nit gehittet")
+                ++i;
+            } else {
+                console.log("Anmeldung erfolgreich")
+                return;                
+            }
+        }    
+    })    
     console.log(req.body.username)
     console.log(req.body.pswdli)
     res.redirect(302, "http://localhost:36187/")
@@ -64,22 +84,67 @@ app.get("/register", (req, res) => {
 app.get("/register/loginregister.css", (req, res) => {
     res.sendFile("Client/loginRegister/loginregister.css", {root: "./"})
 })
-app.post("/register", (req, res) =>{
+/* app.post("/register", (req, res) =>{
     let username = req.body.username
     let password = req.body.pswdli
     let email = req.body.email
 
-    let sql = "INSERT INTO benutzer (`email`, `username`, `passwort`) VALUES ('"+email+"', '"+username+"', '"+password+"')";
-    con.query(sql, function (err, result){
-        if (err) throw err;
-        console.log("1 record inserted");
-    })
+    con.query("SELECT `username`, `email` FROM benutzer", function (err, result, fields) {
+        if(err) throw err;
+        while(i>=0){
+            console.log(i)
+            if(typeof(result[i]) == 'undefined'){
+                break;
+            }
+            if(result[i].username !== username && result[i].email !== email){
+                console.log(result[i].username)
+                console.log("hot nit gehittet")
+                --i; 
+            } else {
+                console.log("Nomegalul")
+                return;
+                           
+            }
+        }    
+        
+        let sql = "INSERT INTO benutzer (`email`, `username`, `passwort`) VALUES ('"+email+"', '"+username+"', '"+password+"')";
+        con.query(sql, function (err, result){
+            if (err) throw err;
+            console.log("1 record inserted");
+        })    
+    });
 
     console.log(req.body.username)
     console.log(req.body.pswdli)
     console.log(req.body.email)
     res.redirect(302, "http://localhost:36187/")
 })
+
+*/
+app.post("/register", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.pswdli;
+  let email = req.body.email;
+
+  if (!username || !password || !email) return res.sendStatus(400);
+
+  checkUsername(username, (username) => {
+    if (!username) return res.redirect(302, "http://localhost:36187/");
+    let sql = "INSERT INTO benutzer (`email`, `username`, `passwort`) VALUES ('" + email + "', '" + username + "', '" + password + "')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      return res.redirect(302, "http://localhost:36187/");
+    });
+  });
+});
+
+const checkUsername = (username, createUser) => {
+  con.query("SELECT `username` FROM benutzer WHERE `username` = ?", [username], function (err, result, fields) {
+    if (err) throw err;
+    if (result?.length > 0) return createUser(undefined);
+    createUser(username);
+  });
+};
 
 
 //classic
@@ -116,11 +181,10 @@ app.post("/classic", (req, res) => {
     let username = "Azir Bot" //req.body.username
     let spielid = 1
 
-
     let sql = "INSERT INTO spielmodi (`anschlaege`, `anschlaegeProSekunde`, `trefferquote`, `spielid`, `zeit`, `username`) VALUES ('"+score+"', '"+anschlaegeProSekunde+"', '100', '"+spielid+"', '00:00:10', '"+username+"' )";
     con.query(sql, function (err, result){
         if (err) throw err;
-        console.log("1 record inserted");
+        console.log("1 record inserted")
     })
 
     res.send("hoi, hon gekreag, jo!!!")
